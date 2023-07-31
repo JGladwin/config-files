46788ac9c61fc27b788efcaadc2eca9a74c0b8b6
Gladwin J <gladwinjoseph6174@protonmail.com>
2023-07-31 19:04:58 +0530

    Added neovim config

---
+++ after/plugin/catppuccin.lua
@@ -0,0 +1,30 @@
+require("catppuccin").setup({
+    color_overrides = {
+        mocha = {
+          text = "#F4CDE9",
+          subtext1 = "#DEBAD4",
+          subtext0 = "#C8A6BE",
+          overlay2 = "#B293A8",
+          overlay1 = "#9C7F92",
+          overlay0 = "#866C7D",
+          surface2 = "#705867",
+          surface1 = "#5A4551",
+          surface0 = "#44313B",
+
+          base = "#352939",
+          mantle = "#211924",
+          crust = "#1a1016",
+        },
+    },
+    integrations = {
+        harpoon = true,
+        mason = true,
+        telescope = {
+            enabled = true,
+        }
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
