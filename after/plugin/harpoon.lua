a11bc1970667b6b91f440f61f48c1ffe8a63e838
Gladwin J <gladwinjoseph6174@protonmail.com>
2023-08-01 21:09:03 +0530

    Few changes to shorcuts for Harpoon and disabled one functionality of telescope for git files?

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,7 +4,7 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-f>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-p>", ui.nav_prev)
+vim.keymap.set("n", "<C-n>", ui.nav_next)
+-- I don't know how to do for C-l for last file .nav_file(-1) ?
